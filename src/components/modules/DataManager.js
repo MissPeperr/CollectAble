0ef1209f2198b3e825e6849068ee883d77b39512
Madi Peper <madisonpeper@gmail.com>
2019-08-29 13:29:10 -0500

    added correct URL for json

--- src/components/modules/DataManager.js
+++ src/components/modules/DataManager.js
@@ -1,4 +1,5 @@
-const remoteURL = "http://localhost:5002"
+// const remoteURL = "http://localhost:5002"
+const remoteURL = "https://collect-able-server.herokuapp.com"
 
 export default Object.create(null, {
     get: {
